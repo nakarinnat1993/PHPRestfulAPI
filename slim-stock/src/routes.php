2bd6b50840166d0742f693922971539d80b1afa5
nakarinnat <nakarinnat1993@gmail.com>
2020-04-25 11:30:38 +0700

    Create method restfulapi post put delete

--- slim-stock/src/routes.php
+++ slim-stock/src/routes.php
@@ -34,6 +34,7 @@ return function (App $app) {
         $app->delete('/data', function (Request $request, Response $response, array $args) use ($container) {
             echo "Group Delete page";
         });
+
         $app->get('/products', function (Request $request, Response $response, array $args) use ($container) {
             $sql = "SELECT * FROM products";
             $stmt = $this->db->prepare($sql);
@@ -52,6 +53,111 @@ return function (App $app) {
                     'data' => $products
                 ];
             }
+            return $this->response->withJson($input);
+        });
+        $app->get('/products/{id}', function (Request $request, Response $response, array $args) use ($container) {
+            $sql = "SELECT * FROM products WHERE id='$args[id]'";
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            $products = $stmt->fetchAll();
+            if (count($products)) {
+                $input = [
+                    'status' => 'Success',
+                    'message' => 'Read product success',
+                    'data' => $products
+                ];
+            } else {
+                $input = [
+                    'status' => 'Fail',
+                    'message' => 'Empty product',
+                    'data' => $products
+                ];
+            }
+            return $this->response->withJson($input);
+        });
+        $app->post('/products', function (Request $request, Response $response, array $args) use ($container) {
+            $body = $this->request->getParsedBody();
+            // print_r($body);
+            $img = "noimg.jpg";
+            $sql = "INSERT INTO products(product_name,product_detail,product_barcode,product_price,product_qty,product_image) 
+                        VALUES(:product_name,:product_detail,:product_barcode,:product_price,:product_qty,:product_image)";
+            $sth = $this->db->prepare($sql);
+            $sth->bindParam("product_name", $body['product_name']);
+            $sth->bindParam("product_detail", $body['product_detail']);
+            $sth->bindParam("product_barcode", $body['product_barcode']);
+            $sth->bindParam("product_price", $body['product_price']);
+            $sth->bindParam("product_qty", $body['product_qty']);
+            $sth->bindParam("product_image", $img);
+
+            if ($sth->execute()) {
+                $data = $this->db->lastInsertId();
+                $input = [
+                    'id' => $data,
+                    'status' => 'success'
+                ];
+            } else {
+                $input = [
+                    'id' => '',
+                    'status' => 'fail'
+                ];
+            }
+
+            return $this->response->withJson($input);
+        });
+        $app->put('/products/{id}', function (Request $request, Response $response, array $args) use ($container) {
+            // รับจาก Client
+            $body = $this->request->getParsedBody();
+
+            $sql = "UPDATE  products SET 
+                               product_name=:product_name,
+                               product_detail=:product_detail,
+                               product_barcode=:product_barcode,
+                               product_price=:product_price,
+                               product_qty=:product_qty
+                           WHERE id='$args[id]'";
+
+            $sth = $this->db->prepare($sql);
+            $sth->bindParam("product_name", $body['product_name']);
+            $sth->bindParam("product_detail", $body['product_detail']);
+            $sth->bindParam("product_barcode", $body['product_barcode']);
+            $sth->bindParam("product_price", $body['product_price']);
+            $sth->bindParam("product_qty", $body['product_qty']);
+
+
+            if ($sth->execute()) {
+                $data = $args['id'];
+                $input = [
+                    'id' => $data,
+                    'status' => 'success'
+                ];
+            } else {
+                $input = [
+                    'id' => '',
+                    'status' => 'fail'
+                ];
+            }
+
+            return $this->response->withJson($input);
+        });
+        $app->delete('/products/{id}', function (Request $request, Response $response, array $args) use ($container) {
+            // รับจาก Client
+            $sql = "DELETE FROM products WHERE id='$args[id]'";
+
+            $sth = $this->db->prepare($sql);
+
+            if ($sth->execute()) {
+                $data = $args['id'];
+                $input = [
+                    'id' => $data,
+                    'status' => 'success'
+                ];
+            } else {
+                $input = [
+                    'id' => '',
+                    'status' => 'fail'
+                ];
+            }
+
             return $this->response->withJson($input);
         });
     });
